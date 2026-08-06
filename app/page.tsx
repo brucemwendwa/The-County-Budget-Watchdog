0affac91f56c5f260b8406a6de8d7fd625c6a4a1
brucemwendwa <brucemwendwa915@email.com>
2026-08-06 13:32:16 +0300

    Remove unused data fetching in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { LandingPage } from "@/components/landing-page";
-import { getAdminDashboardData } from "@/lib/data";
 
 export default function Home() {
-  const data = getAdminDashboardData();
-  return <LandingPage departments={data.departments} smsPreview={data.digests[0]} />;
+  return <LandingPage />;
 }
